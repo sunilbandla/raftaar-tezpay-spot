072bfca7798f7a007d7e2e58e374fc65ed6eb435
Sunil B <sunilbandla@gmail.com>
2021-02-04 16:57:09 +0530

    Add payer image holder

--- src/components/Orders.svelte
+++ src/components/Orders.svelte
@@ -5,23 +5,50 @@
 </script>
 
 <div class="orders-list">
-    {#each recent_transactions.transactions as order}
-  <div class="order">
-      <OrderHeader {order} />
-      <OrderBody {order} />
-  </div>
-    {/each}
+  {#each recent_transactions.transactions as order}
+    <div class="order">
+      <div class="order-image">
+        <div class="order-image-content">
+          {order.payerName.charAt(0)}
+        </div>
+      </div>
+      <div class="order-content">
+        <OrderHeader {order} />
+        <OrderBody {order} />
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style>
   .orders-list {
-    /* border: 5px solid lightblue; */
-    /* border-radius: 4px; */
-    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%);
+    box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%),
+      0 1px 5px 0 rgb(0 0 0 / 12%);
   }
-
+  .order-content {
+    padding: 5px 15px;
+    width: 100%;
+  }
+  .order-image-content {
+    margin: auto 22px;
+    text-align: center;
+  }
+  .order-image {
+    background-color: rgb(55, 110, 228);
+    box-shadow: 0 0 6px 0 rgb(0 0 0 / 20%);
+    color: white;
+    border-radius: 50%;
+    flex-shrink: 0;
+    object-fit: cover;
+    background-size: cover;
+    height: 50px;
+    width: 50px;
+    margin: 10px;
+    display: flex;
+  }
   .order {
     border-bottom: 1px solid lightgray;
-    padding: 15px;
+    display: flex;
+    flex-direction: row;
   }
 </style>
